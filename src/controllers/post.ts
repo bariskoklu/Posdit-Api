7234a122943cf7b90dbf370854a3ea1a494cef91
bariskoklu <30669414+bariskoklu@users.noreply.github.com>
2024-03-16 23:09:22 +0300

    added upvotePost endpoint

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -82,6 +82,66 @@ export const updatePost = async (req: Request, res: Response) => {
   });
 };
 
+// @desc    upvote post
+// @route   PUT /api/posts/upvote/:id
+// @access  private
+export const upvotePost = async (req: Request, res: Response) => {
+  var [error, post] = await Result(PostModel.findById(req.params.id));
+
+  if (error) {
+    return res.status(400).json({
+      success: true,
+      error: error.message,
+    });
+  }
+
+  if (!post) {
+    return res.status(404).json({
+      success: false,
+      error: `Post not found with id of ${req.params.id}`,
+    });
+  }
+
+  var userIndex = post.downvotes.indexOf(req.user._id);
+
+  if (userIndex > -1) {
+    post.downvotes.splice(userIndex, 1);
+  }
+
+  userIndex = post.upvotes.indexOf(req.user._id);
+
+  if (userIndex > -1) {
+    return res.status(404).json({
+      success: false,
+      error: `This post is already upvoted by the user`,
+    });
+  }
+
+  [error, post] = await Result(PostModel.findByIdAndUpdate(req.params.id, post, {
+    new: true,
+    runValidators: true,
+  }));
+
+  if (error) {
+    return res.status(400).json({
+      success: true,
+      error: error.message,
+    });
+  }
+
+  if (!post) {
+    return res.status(404).json({
+      success: false,
+      error: `Post not found with id of ${req.params.id}`,
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    data: post,
+  });
+};
+
 // @desc    delete post
 // @route   DELETE /api/posts/:id
 // @access  private
