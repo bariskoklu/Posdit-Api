28b7703af9fd634f10163b72700d3e69628ac244
bariskoklu <30669414+bariskoklu@users.noreply.github.com>
2024-03-20 19:42:20 +0300

    added endpoints for upvote and downvoting posts. Also added atlas search functionality for posts

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -5,20 +5,57 @@ import { Result } from "../helpers";
 // @desc    create post
 // @route   POST /api/posts
 // @access  private
-export const createPost =
-  async (req: Request, res: Response) => {
-    const post = await PostModel.create(req.body);
-    return res.status(200).json({
-      success: true,
-      data: post,
-    });
-  };
+export const createPost = async (req: Request, res: Response) => {
+
+  const searchQuery = req.params.query;
+  console.log(searchQuery);
+  const pipeline = [];
+
+  pipeline.push({
+    $search: {
+      index: 'posts',
+      text: {
+        query: searchQuery,
+        path: ['context', 'title'],
+        fuzzy: {},
+      },
+    },
+  });
+  const post = await PostModel.create(req.body);
+  return res.status(200).json({
+    success: true,
+    data: post,
+  });
+};
 
 // @desc    show all posts
 // @route   GET /api/posts
 // @access  public
 export const getPosts = async (req: Request, res: Response) => {
-  const posts = await PostModel.find();
+  const searchQuery = req.query.query;
+  var posts: IPost[];
+  var error: Error;
+
+  if (searchQuery) {
+    const pipeline = [];
+
+    pipeline.push({
+      $search: {
+        index: 'posts',
+        text: {
+          query: searchQuery,
+          path: ['context', 'title'],
+          fuzzy: {},
+        },
+      },
+    });
+
+    [error, posts] = await Result(PostModel.aggregate(pipeline));
+
+  }
+  else {
+    [error, posts] = await Result(PostModel.find());
+  }
 
   return res.status(200).json({
     success: true,
@@ -35,7 +72,7 @@ export const getPost = async (req: Request, res: Response) => {
 
   if (error) {
     return res.status(400).json({
-      success: true,
+      success: false,
       error: error.message,
     });
   }
@@ -64,7 +101,7 @@ export const updatePost = async (req: Request, res: Response) => {
 
   if (error) {
     return res.status(400).json({
-      success: true,
+      success: false,
       error: error.message,
     });
   }
@@ -83,14 +120,14 @@ export const updatePost = async (req: Request, res: Response) => {
 };
 
 // @desc    upvote post
-// @route   PUT /api/posts/upvote/:id
+// @route   PUT /api/posts-upvote/:id
 // @access  private
 export const upvotePost = async (req: Request, res: Response) => {
   var [error, post] = await Result(PostModel.findById(req.params.id));
 
   if (error) {
     return res.status(400).json({
-      success: true,
+      success: false,
       error: error.message,
     });
   }
@@ -124,7 +161,68 @@ export const upvotePost = async (req: Request, res: Response) => {
 
   if (error) {
     return res.status(400).json({
-      success: true,
+      success: false,
+      error: error.message,
+    });
+  }
+
+  if (!post) {
+    return res.status(404).json({
+      success: false,
+      error: `Post not found with id of ${req.params.id}`,
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    data: post,
+  });
+};
+
+// @desc    downvote post
+// @route   PUT /api/posts-downvote/:id
+// @access  private
+export const downvotePost = async (req: Request, res: Response) => {
+  var [error, post] = await Result(PostModel.findById(req.params.id));
+
+  if (error) {
+    return res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+
+  if (!post) {
+    return res.status(404).json({
+      success: false,
+      error: `Post not found with id of ${req.params.id}`,
+    });
+  }
+
+  var userIndex = post.downvotes.indexOf(req.user._id);
+
+  if (userIndex > -1) {
+
+    return res.status(404).json({
+      success: false,
+      error: `This post is already downvoted by the user`,
+    });
+  }
+
+  userIndex = post.upvotes.indexOf(req.user._id);
+
+  if (userIndex > -1) {
+    post.upvotes.splice(userIndex, 1);
+  }
+
+  [error, post] = await Result(PostModel.findByIdAndUpdate(req.params.id, post, {
+    new: true,
+    runValidators: true,
+  }));
+
+  if (error) {
+    return res.status(400).json({
+      success: false,
       error: error.message,
     });
   }
@@ -150,7 +248,7 @@ export const deletePost = async (req: Request, res: Response) => {
 
   if (error) {
     return res.status(400).json({
-      success: true,
+      success: false,
       error: error.message,
     });
   }
--- src/router/post.ts
+++ src/router/post.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { createPost, getPost, getPosts, updatePost, deletePost } from "../controllers/post";
+import { createPost, getPost, getPosts, updatePost, deletePost, upvotePost, downvotePost } from "../controllers/post";
 import { IsAuthenticated } from "../middleware/auth";
 
 export default (router: express.Router) => {
@@ -8,5 +8,7 @@ export default (router: express.Router) => {
   router.get('/api/posts', IsAuthenticated, getPosts);
   router.get('/api/posts/:id', IsAuthenticated, getPost);
   router.put('/api/posts/:id', IsAuthenticated, updatePost);
+  router.put('/api/postsupvote/:id', IsAuthenticated, upvotePost);
+  router.put('/api/postsdownvote/:id', IsAuthenticated, downvotePost);
   router.delete('/api/posts/:id', IsAuthenticated, deletePost);
 };
